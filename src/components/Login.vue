9b81e9dd6045b5d738739c7304ab99a1b128ac95
branches <contacto@branches.com.mx>
2020-02-07 18:03:02 -0600

    pone textos en pantalla login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,49 +5,44 @@
 			<div class="row mx-0">
 				<div class="col-md-7 order-md-2 signin-right-column px-5 bg-dark">
 					<a class="signin-logo d-sm-block d-md-none" href="#">
-						<img src="assets/img/logo-white.png" width="145" height="32.3" alt="QuillPro">
+						<img src="assets/img/logo-white.png" width="145" height="32.3" v-bind:alt="Sistema">
 					</a>
-					<h1 class="display-4">Sign In To get Started</h1>
+					<h1 class="display-4">{{Sistema}}</h1>
 					<p class="lead mb-5">
-						Big data latte SpaceTeam unicorn cortado hacker physical computing paradigm.
+						Ingresa tu usuario y contraseña.
 					</p>
 				</div>
 				<div class="col-md-5 order-md-1 signin-left-column bg-white px-5">
 					<a class="signin-logo d-sm-none d-md-block" href="#">
-						<img src="assets/img/logo-dark.png" width="145" height="32.3" alt="QuillPro">
+						<img src="assets/img/logo-dark.png" width="145" height="32.3" v-bind:alt="Sistema">
 					</a>
 					<form>
 						<div class="form-group">
-							<label for="exampleInputEmail1">Username </label>
-							<input v-model="Username" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-							<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+							<label for="user">Usuario </label>
+							<input v-model="Username" type="text" class="form-control" id="user" placeholder="Usuario">
+							
 						</div>
 						<div class="form-group">
-							<label for="exampleInputPassword1">Password</label>
-							<input v-model="Password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-						</div>
-						<div class="custom-control custom-checkbox mb-3">
-							<input type="checkbox" class="custom-control-input" id="keep-signed-in">
-							<label class="custom-control-label" for="keep-signed-in">Keep Me Signed In</label>
+							<label for="password">Contraseña</label>
+							<input v-model="Password" type="password" class="form-control" id="password" placeholder="Contraseña">
 						</div>
 						
-						<button type="button" id="btna" class="btn btn-primary btn-block" @click="login" >
+						
+						<button type="button" id="btnLogin" class="btn btn-primary btn-block" @click="login" >
 							<i class="batch-icon batch-icon-key"></i>
-							Sign In
+							Ingresar
 						</button>
 
 						<button type="button" id="btnb" class="btn btn-primary  btn-block" @click="logout" >
 							<i class="batch-icon batch-icon-key"></i>
 							logout
 						</button>
-						<button type="button" class="btn btn-primary  btn-block" @click="showToken" >
+						<button type="button" id="btnc" class="btn btn-primary  btn-block" @click="showToken" >
 							<i class="batch-icon batch-icon-key"></i>
 							showtoken
 						</button>
 						
-						<p class="text-center">
-							Don't Have An Account? <a href="sisu-signup.html">Sign Up here</a>
-						</p>
+						
 					</form>
 				</div>
 			</div>
@@ -65,6 +60,7 @@ import MasterModel from '@/modules/core/MasterModel.js';
 export default {
   data() {
     return {
+		Sistema: 'Nombre Sistema',
       Username: 'root',
       Password: 'root',
     };
