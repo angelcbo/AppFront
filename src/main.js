72282db586c88cf85374fff275d233154aa8f52c
AngelC <omar.ceniceros.berumen.18@gmail.com>
2020-11-03 21:37:10 -0600

    removed unsued imports

--- src/main.js
+++ src/main.js
@@ -7,34 +7,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import VueHtmlToPaper from 'vue-html-to-paper';
+
 
 const APP_DEV = process.env.VUE_APP_DEV == 'true';
 
 Vue.use(Vuelidate);
 
-let basePath= 'http://localhost:8080/css';
-const options = {
-  name: '_blank',
-  specs: [
-    'fullscreen=yes',
-    'titlebar=yes',
-    'scrollbars=yes'
-  ],
-  
-  styles: [
-    "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css",
-    "https://unpkg.com/kidlat-css/css/kidlat.css",
-    `${basePath}/boostrap.min.css`,
-    `${basePath}/style.css`,
-    `${basePath}/my_style.css`
-  //   "./boostrap.min.css",
-  //    "./style.css",
-  //    "./my_style.css",
-  ]
-}
-
-Vue.use(VueHtmlToPaper);
 
 Vue.prototype.$http = Axios;
 Vue.prototype.$faker = Faker;
